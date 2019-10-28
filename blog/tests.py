3c3796cd82c23a99fc033de6e2d66f82ff61e721
Guy Strelitz <guystrelitz@users.noreply.github.com>
2019-10-28 19:11:42 +0000

    CRUD and user accounts

--- blog/tests.py
+++ blog/tests.py
@@ -26,6 +26,10 @@ class BlogTests(TestCase):
     self.assertEqual(str(post), post.title)
 
 
+  def test_get_absolute_url(self):
+    self.assertEqual(self.post.get_absolute_url(), '/post/1/')
+
+
   def test_post_content(self):
     self.assertEqual(f'{self.post.title}', 'A good title')
     self.assertEqual(f'{self.post.body}', 'Nice body content')
@@ -38,6 +42,7 @@ class BlogTests(TestCase):
     self.assertContains(response, 'Nice body content')
     self.assertTemplateUsed(response, 'home.html')
 
+
   def test_post_detail_view(self):
     response = self.client.get('/post/1/')
     self.assertEqual(response.status_code, 200)
@@ -45,3 +50,27 @@ class BlogTests(TestCase):
 
     no_response = self.client.get('/post/10101/')
     self.assertEqual(no_response.status_code, 404)
+
+
+  def test_post_create_view(self):
+    response = self.client.post(reverse('post_new'), {
+      'title': 'New title',
+      'body': 'New text',
+      'author': self.user
+    })
+    self.assertEqual(response.status_code, 200)
+    self.assertContains(response, 'New title')
+    self.assertContains(response, 'New text')
+
+
+  def test_post_update_view(self):
+    response = self.client.post(reverse('post_edit', args='1'), {
+      'title': 'Updated title',
+      'body': 'Updated text'
+    })
+    self.assertEqual(response.status_code, 302)
+
+
+  def test_post_delete_view(self):
+    response = self.client.post(reverse('post_edit', args='1'))
+    self.assertEqual(response.status_code, 200)
